d00770d1df0d2346b9bf52c29ffb1384f0efca92
Jérémie Crinon <jeremie.crinon@gmail.com>
2023-07-11 10:11:30 +0200

    Making the verification of the information of the contact form in the PHP file (I forgot to do it)

--- send_mail.php
+++ send_mail.php
@@ -4,6 +4,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["form_email"];
     $message = $_POST["form_message"];
 
+    if (empty($nom) || empty($message)) {
+        exit;
+    }
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false ) {
+        exit;
+    }
+
     $result = "E-mail automatique venant du formulaire de contact de jcrinon.com.\nNom: " . $nom . "\nE-mail: " . $email . "\nMessage:\n" . $message;  
 
     if (mail("contact@jcrinon.com", "Message automatique jcrinon.com", $result)) {
