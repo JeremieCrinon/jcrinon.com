8ff36a382f673d00f906a42ef8806863611283eb
Jérémie Crinon <jeremie.crinon@gmail.com>
2023-07-11 09:53:16 +0200

    Finishing the contact form. Making the PHP file to send the mail.

--- send_mail.php
+++ send_mail.php
@@ -1,8 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $nom = $_POST["contact--main--contact_form--form--name"];
+    $nom = $_POST["form_name"];
+    $email = $_POST["form_email"];
+    $message = $_POST["form_message"];
 
-    header('Location: confirmation.html');
+    $result = "E-mail automatique venant du formulaire de contact de jcrinon.com.\nNom: " . $nom . "\nE-mail: " . $email . "\nMessage:\n" . $message;  
+
+    if (mail("contact@jcrinon.com", "Message automatique jcrinon.com", $result)) {
+        header('Location: confirmation.html');
+    } else {
+        header('Location: fail.html');
+    }
     exit;
 }
 ?>
