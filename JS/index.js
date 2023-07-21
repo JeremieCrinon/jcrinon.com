c920b1023624aae21bdf78412fb68b50e6f34c77
Jérémie Crinon <jeremie.crinon@gmail.com>
2023-07-21 10:21:34 +0200

    Upgrading the header

--- JS/index.js
+++ JS/index.js
@@ -4,29 +4,31 @@ const header__nav__ul__element__2 = document.querySelector('.header--nav--ul--el
 const header__nav__ul__element__3 = document.querySelector('.header--nav--ul--element--3 a');
 const header__nav__ul__actual = document.querySelector('.header--nav--actual');
 
-header__nav__ul__element__1.addEventListener('mouseover', function() {
-    header__nav__ul__actual.classList.add('remove_after');
-});
+if (header__nav__ul__actual) {
+    header__nav__ul__element__1.addEventListener('mouseover', function() {
+        header__nav__ul__actual.classList.add('remove_after');
+    });
 
-header__nav__ul__element__2.addEventListener('mouseover', function() {
-    header__nav__ul__actual.classList.add('remove_after');
-});
+    header__nav__ul__element__2.addEventListener('mouseover', function() {
+        header__nav__ul__actual.classList.add('remove_after');
+    });
 
-header__nav__ul__element__3.addEventListener('mouseover', function() {
-    header__nav__ul__actual.classList.add('remove_after');
-});
+    header__nav__ul__element__3.addEventListener('mouseover', function() {
+        header__nav__ul__actual.classList.add('remove_after');
+    });
 
-header__nav__ul__element__1.addEventListener('mouseout', function() {
-    header__nav__ul__actual.classList.remove('remove_after');
-});
+    header__nav__ul__element__1.addEventListener('mouseout', function() {
+        header__nav__ul__actual.classList.remove('remove_after');
+    });
 
-header__nav__ul__element__2.addEventListener('mouseout', function() {
-    header__nav__ul__actual.classList.remove('remove_after');
-});
+    header__nav__ul__element__2.addEventListener('mouseout', function() {
+        header__nav__ul__actual.classList.remove('remove_after');
+    });
 
-header__nav__ul__element__3.addEventListener('mouseout', function() {
-    header__nav__ul__actual.classList.remove('remove_after');
-});
+    header__nav__ul__element__3.addEventListener('mouseout', function() {
+        header__nav__ul__actual.classList.remove('remove_after');
+    });
+}
 // Enlever le soulignage de la page actuelle dans le header quand on passe en hover sur une autre page
 
 // On vérifie le formulaire avant d'appeller le PHP
@@ -55,3 +57,26 @@ function formVerif(event) {
 }
 
 // On vérifie le formulaire avant d'appeller le PHP
+
+// On mets le inside_burger en display: block quand le burger est cliqué
+const burger = document.querySelector('.header--burger');
+const inside_burger = document.querySelector('.header--inside_burger');
+const burger_bar_1 = document.querySelector('.header--burger div:nth-of-type(1)');
+const burger_bar_2 = document.querySelector('.header--burger div:nth-of-type(3)');
+
+burger.addEventListener('click', function() {
+    if (inside_burger.classList.contains('header--inside_burger--active')) {
+        inside_burger.classList.remove('header--inside_burger--active');
+        burger_bar_1.classList.remove('pos-absolute')
+        burger_bar_2.classList.remove('pos-absolute')
+        burger.classList.remove('header--cross');
+    } else {
+        inside_burger.classList.add('header--inside_burger--active');
+        burger.classList.add('header--cross');
+        setTimeout(function() {
+            burger_bar_1.classList.add('pos-absolute');
+            burger_bar_2.classList.add('pos-absolute');
+        }, 200);
+    }
+});
+// On mets le inside_burger en display: block quand le burger est cliqué
